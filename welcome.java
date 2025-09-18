fd5374fee9e0c3d83234f2dde232831fee3bc250
suagrtts <kiethdaryll1@gmail.com>
2025-09-18 22:32:25 +0800

    student info in c
    structures

--- welcome.java
+++ welcome.java
@@ -1,7 +1,57 @@
-public class welcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome to The Land of Gods!");
-        System.out.println("I am Zeus, the king of gods.");
-        System.out.print("Go kill yourself.");
-    }
+#include<stdio.h>
+#include<string.h>
+
+struct Course {
+    char courseName[100];
+    int units;
+};
+
+struct Student {
+    char studentName[100];
+    int idNum;
+    struct Course enrolled;
+};
+
+void inputStudent(Student *s){
+    printf("Enter your name: ");
+    fgets(s->studentName, sizeof(s->studentName), stdin);
+
+    printf("Enter your ID number: ");
+    scanf("%d", &s->idNum);
 }
+
+void inputCourse(Course *c){
+    printf("Enter the course name: ");
+    fgets(c->courseName, sizeof(c->courseName), stdin);
+
+    printf("Enter the number of units: ");
+    scanf("%d", &c->units);
+}
+
+void enrollStudent(Student *s, Course *c){
+    strcpy(s->enrolled, courseName);
+    s->enrolled = c->units;
+}
+
+void updateCourse(Course *c, char *newCourse, int newUnits){
+    strcpy(*newCourse, c->courseName);
+    c->units = newUnits;
+}
+
+void display(Student s){
+    printf("Name: %s\n", s.studentName);
+    printf("ID: %d\n", s.idNum);
+    printf("Enrolled in: %s (%d units)", s->enrolled.courseName, s->enrolled.units);
+}
+
+int main() {
+    struct Student s;
+
+    inputStudent(&s);
+    inputCourse(&c);
+    enrollStudent(&s, &c);
+    display(s);
+    updateCourse(&c, "Networking 1", 4);
+    display(s);
+}
+
